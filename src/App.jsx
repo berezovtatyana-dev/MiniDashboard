70b1606a643c49b7c9922ac8e701e936ce932702
Таня <berezov.tatyana@gmail.com>
2026-06-03 20:37:28 +0700

    Filtered

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,32 @@ import AnaliticTable from "./AnaliticTable";
 function App() {
   const [rowData, setRowData] = useState(()=>generateMockData());
   const [filters, setFilters] = useState({search: '', category: '', status: ''});
+  const [isModalOpen, setModalOpen] = useState(false)
 
+  //фильтрация массива
   const filteredData = useMemo(()=>{
+    return rowData.filter(item => {
+      const matchesSearch = item.title.toLowerCase().includes(filters.search.toLowerCase())
+      const matchesCategory = filters.category ? item.category === filters.category : true;
+      const matchesStatus = filters.status ? item.status === filters.status : true;
+
+      return matchesSearch && matchesCategory && matchesStatus
+    }
+    )
 
   },[rowData, filters.search, filters.category, filters.status]);
-
+  const handleNewItem = ()=> {};
   return (
     <>
-      <DashboardMetrics filteredData={rowData} />
-
+    <h1>Dashboard Builder</h1>
+    {/* компонент метрик */}
+      <DashboardMetrics filteredData={filteredData} />
+      {/* панель фильтров */}
+      <FilterPanel
+        filters={filters}
+        setFilters={setFilters}
+        onOpenModal={handleNewItem}/>
+        <AnaliticTable data={filteredData} />
     </>
   )
 }
